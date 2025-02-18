8f6dc1d5cc47d97960f636ba5dc291890e486829
tranvuluan2270 <tranvuluan2270@gmail.com>
2025-02-18 15:13:43 +0700

    Move example code to separate files in the examples directory

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
+import "go-tutorial/examples"
 func main() {
-	//HelloWorld()
-	// Values()
-	//Variables()
-	//Constants()
-	//For()
-	//IfElse()
-	Switch()
+	// examples.HelloWorld()
+	// examples.Values()
+	// examples.Variables()
+	// examples.Constants()
+	// examples.For()
+	// examples.IfElse()
+	 examples.Switch()
 }
