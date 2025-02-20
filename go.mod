34804518c52ea47b4185abafbca72d815e4d4a55
tranvuluan2270 <58285514+tranvuluan2270@users.noreply.github.com>
2025-02-20 16:40:57 +0700

    Add Gorilla Mux for routing and implement basic HTTP handlers

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-tutorial
 
 go 1.24.0
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,8 +1,42 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 
+	"github.com/gorilla/mux"
 )
+
+// User table struct
+// type User struct {
+// 	Id      primitive.ObjectID `json:"id" bson:"_id"`
+// 	Name    string             `json:"name" bson:"name"`
+// 	Email   string             `json:"email" bson:"email"`
+// 	Age     int                `json:"age" bson:"age"`
+// 	Address string             `json:"address" bson:"address"`
+// }
+
 func main() {
-	
+
+	// default router (net/http)
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	// goriila/mux router
+	router := mux.NewRouter()
+	router.HandleFunc("/", rootHandler).Methods("GET")       // GET request
+	router.HandleFunc("/hello", helloHandler).Methods("GET") // GET request
+
+	// start the server on port 8080
+	http.ListenAndServe(":8080", router)
+	// nil parameter is used to use the default router (net/http)
+	// router parameter is used to use the gorilla/mux router
+
+}
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to the homepage!")
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
 }
