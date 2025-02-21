5dcc8806e500f88444d6a7d5a7a7be8a67bb00e7
tranvuluan2270 <58285514+tranvuluan2270@users.noreply.github.com>
2025-02-21 16:54:04 +0700

    Add MongoDB integration and user retrieval endpoint

--- main.go
+++ main.go
@@ -1,42 +1,91 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const uri = "mongodb://localhost:27017/"
+
+var client *mongo.Client
+var err error
+
 // User table struct
-// type User struct {
-// 	Id      primitive.ObjectID `json:"id" bson:"_id"`
-// 	Name    string             `json:"name" bson:"name"`
-// 	Email   string             `json:"email" bson:"email"`
-// 	Age     int                `json:"age" bson:"age"`
-// 	Address string             `json:"address" bson:"address"`
-// }
+type User struct {
+	ID      string `json:"id" bson:"_id"`
+	Name    string `json:"name" bson:"name"`
+	Email   string `json:"email" bson:"email"`
+	Age     int    `json:"age" bson:"age"`
+	Address string `json:"address" bson:"address"`
+}
 
 func main() {
+	// Initialize MongoDB client
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err = mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(context.TODO())
 
-	// default router (net/http)
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/hello", helloHandler)
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
 
-	// goriila/mux router
+	// Initialize Gorilla Mux router
 	router := mux.NewRouter()
-	router.HandleFunc("/", rootHandler).Methods("GET")       // GET request
-	router.HandleFunc("/hello", helloHandler).Methods("GET") // GET request
+	router.HandleFunc("/", helloHandler).Methods("GET")            // GET request
+	router.HandleFunc("/users", getAllUsersHandler).Methods("GET") // GET request
 
-	// start the server on port 8080
-	http.ListenAndServe(":8080", router)
+	// Start the server on port 8080
 	// nil parameter is used to use the default router (net/http)
 	// router parameter is used to use the gorilla/mux router
+	fmt.Println("Server running at http://localhost:80")
+	log.Fatal(http.ListenAndServe(":80", router))
 
 }
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome to the homepage!")
-}
-
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
+	fmt.Fprintln(w, "Hello World!")
+}
+
+func getAllUsersHandler(w http.ResponseWriter, r *http.Request) {
+	// Access the users collection from the database
+	usersCollection := client.Database("test-db").Collection("users")
+
+	// Get all users from the users collection
+	cursor, err := usersCollection.Find(context.TODO(), bson.M{})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer cursor.Close(context.TODO())
+
+	var users []User
+
+	for cursor.Next(context.TODO()) {
+		var user User
+		err := cursor.Decode(&user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = append(users, user)
+	}
+	if err := cursor.Err(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
 }
